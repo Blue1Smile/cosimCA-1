ee519d6210d540eaff5248d6c453545a6a821892
dodo <hollykunge@163.com>
2016-12-27 16:29:50 +0800

    修改process为processflow，process与jar包有冲突

--- src/com/casic/datadriver/controller/flow/ProcessFlowController.java
+++ src/com/casic/datadriver/controller/flow/ProcessFlowController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @RequestMapping("/datadriver/designflow/")
-public class ProcessController extends AbstractController {
+public class ProcessFlowController extends AbstractController {
 
 }
