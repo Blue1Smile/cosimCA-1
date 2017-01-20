105f21f5410b8b2c59ea372b734587cda2ff7ade
386845154a <824555324@qq.com>
2017-01-20 15:34:31 +0800

    20170120 To-zh 提交 增加工具管理功能

---
+++ src/com/casic/datadriver/dao/ToolCenterDao.java
@@ -0,0 +1,27 @@
+package com.casic.datadriver.dao;
+
+import com.casic.datadriver.model.tool.ToolCenterModel;
+import com.hotent.core.db.BaseDao;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by 忠 on 2017/1/13.
+ */
+@Repository
+public class ToolCenterDao  extends BaseDao<ToolCenterModel> {
+    @Override
+    public Class getEntityClass() {
+        return ToolCenterModel.class;
+    }
+
+
+    public List<ToolCenterModel> querytoolBymajor(String major) {
+        return this.getBySqlKey("querytoolBymajor", major);
+    }
+
+    public List<ToolCenterModel> querytoolByname(String toolname) {
+        return this.getBySqlKey("querytoolByname", toolname);
+    }
+}
