08e08db53fa9c65d0cbb96a078f45f6d9f848369
blue <1604690210@q.com>
2017-02-22 12:49:56 +0800

    项目list  修改

--- src/com/casic/datadriver/controller/project/ProjectController.java
+++ src/com/casic/datadriver/controller/project/ProjectController.java
@@ -175,9 +175,34 @@ public class ProjectController extends BaseController {
     public ModelAndView queryProjectBasicInfoList(HttpServletRequest request, HttpServletResponse response)
             throws Exception {
         Long userId = ContextUtil.getCurrentUserId();
-//        QueryFilter queryFilter = new QueryFilter(request, "ProjectItem");
         List<Project> projectList = this.projectService.queryProjectBasicInfoList(userId);
 
+        for (int i=0; i<projectList.size();i++){
+            Project nowProject = projectList.get(i);
+            List<ProTaskDependance> proTaskDependanceList=proTaskDependanceService.getProTaskDependanceList(nowProject.getDdProjectId());
+            int listLengthStart=0;
+            int listLengthComplete =0;
+            for(int j=0; j<proTaskDependanceList.size();j++){
+                TaskInfo nowTask= taskInfoService.getById(proTaskDependanceList.get(j).getDdTaskId());
+                if(nowTask.getDdTaskChildType().equals("publishpanel")||nowTask.getDdTaskChildType().equals("checkpanel")){
+                    listLengthStart++;
+                }
+                if(nowTask.getDdTaskChildType().equals("completepanel")){
+                    listLengthComplete++;
+                }
+            }
+            if(listLengthComplete==proTaskDependanceList.size()&&listLengthComplete!=0){
+                projectList.get(i).setDdProjectPhaseId(projectList.get(i).complete);
+            }
+            else{
+                if(listLengthStart>0){
+                    projectList.get(i).setDdProjectPhaseId(projectList.get(i).start);
+                }
+                else{
+                    projectList.get(i).setDdProjectPhaseId(projectList.get(i).unstart);
+                }
+            }
+        }
         ModelAndView mv = this.getAutoView().addObject("projectList",
                 projectList);
         return mv;
@@ -263,59 +288,59 @@ public class ProjectController extends BaseController {
         return getAutoView().addObject("Project", Project);
     }
 
-    /**
-     * 启动项目 dd_project_start
-     *
-     * @param request
-     * @param response
-     * @return
-     * @throws Exception
-     */
-    @RequestMapping("start")
-    @Action(description = "启动项目")
-    public void start(HttpServletRequest request, HttpServletResponse response) throws Exception {
-
-        PrintWriter out = response.getWriter();
-
-        try {
-//			if(!Token.isTokenStringValid(request.getParameter(Token.TOKEN_STRING_NAME), request.getSession())) {
-//				ResultMessage resultMessage = new ResultMessage(
-//						ResultMessage.Fail, "非法访问!");
-//				out.print(resultMessage);
-//			}
-            Long id = RequestUtil.getLong(request, "id");
-
-            ProcessCmd processCmd = BpmUtil.getProcessCmd(request);
-            processCmd.setCurrentUserId(ContextUtil.getCurrentUserId().toString());
-
-            ProjectStartCmd projectStartCmd = new ProjectStartCmd();
-            projectStartCmd.setStartUser(ContextUtil.getCurrentUser());
-            projectStartCmd.setCurrentUser(ContextUtil.getCurrentUser());
-
-            projectStartCmd.setProcessCmd(processCmd);
-            projectStartService.startProject(id, projectStartCmd);
-            //更新项目状态
-            Project project = projectService.getById(id);
-            project.setDdProjectState(Project.STATUS_RUNNING);
-            projectService.update(project);
-            ResultMessage resultMessage = new ResultMessage(
-                    ResultMessage.Success, "启动流程成功!");
-            out.print(resultMessage);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            String str = MessageUtil.getMessage();
-            if (StringUtil.isNotEmpty(str)) {
-                ResultMessage resultMessage = new ResultMessage(
-                        ResultMessage.Fail, "创建业务实例失败:\r\n" + str);
-                out.print(resultMessage);
-            } else {
-                String message = ExceptionUtil.getExceptionMessage(ex);
-                ResultMessage resultMessage = new ResultMessage(
-                        ResultMessage.Fail, message);
-                out.print(resultMessage);
-            }
-        }
-    }
+//    /**
+//     * 启动项目 dd_project_start
+//     *
+//     * @param request
+//     * @param response
+//     * @return
+//     * @throws Exception
+//     */
+//    @RequestMapping("start")
+//    @Action(description = "启动项目")
+//    public void start(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//
+//        PrintWriter out = response.getWriter();
+//
+//        try {
+////			if(!Token.isTokenStringValid(request.getParameter(Token.TOKEN_STRING_NAME), request.getSession())) {
+////				ResultMessage resultMessage = new ResultMessage(
+////						ResultMessage.Fail, "非法访问!");
+////				out.print(resultMessage);
+////			}
+//            Long id = RequestUtil.getLong(request, "id");
+//
+//            ProcessCmd processCmd = BpmUtil.getProcessCmd(request);
+//            processCmd.setCurrentUserId(ContextUtil.getCurrentUserId().toString());
+//
+//            ProjectStartCmd projectStartCmd = new ProjectStartCmd();
+//            projectStartCmd.setStartUser(ContextUtil.getCurrentUser());
+//            projectStartCmd.setCurrentUser(ContextUtil.getCurrentUser());
+//
+//            projectStartCmd.setProcessCmd(processCmd);
+//            projectStartService.startProject(id, projectStartCmd);
+//            //更新项目状态
+//            Project project = projectService.getById(id);
+//            project.setDdProjectState(Project.STATUS_RUNNING);
+//            projectService.update(project);
+//            ResultMessage resultMessage = new ResultMessage(
+//                    ResultMessage.Success, "启动流程成功!");
+//            out.print(resultMessage);
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//            String str = MessageUtil.getMessage();
+//            if (StringUtil.isNotEmpty(str)) {
+//                ResultMessage resultMessage = new ResultMessage(
+//                        ResultMessage.Fail, "创建业务实例失败:\r\n" + str);
+//                out.print(resultMessage);
+//            } else {
+//                String message = ExceptionUtil.getExceptionMessage(ex);
+//                ResultMessage resultMessage = new ResultMessage(
+//                        ResultMessage.Fail, message);
+//                out.print(resultMessage);
+//            }
+//        }
+//    }
 
     /**
      * 项目指标
